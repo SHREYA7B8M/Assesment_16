fb3cce45f172440bffde66be815c0d3ab8cdc9f4
s66829020@gmail.com <s66829020@gmail.com>
2023-09-01 15:44:19 +0530

    first commit

---
+++ Assesment_16/Data/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assesment_16.Models;
+
+namespace Assesment_16.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext (DbContextOptions<TaskDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assesment_16.Models.Tasks> Tasks { get; set; } = default!;
+    }
+}
---
+++ Assesment_16/Models/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assesment_16.Models
+{
+    [Table("Tasks")]
+    public class Tasks
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
